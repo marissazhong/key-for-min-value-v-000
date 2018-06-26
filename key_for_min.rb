17e2cd9ee18874b3bf473e8ba77504a41717cf29
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 13:43:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |name, value|
       if value < min_hash[1]
         min_hash[0], min_hash[1] = name, value
+        puts(min_hash)
       end
       return min_hash[0]
     end
