e4201f8a0b51440dee86adc39574f4e230aa45f8
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 13:38:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,12 @@
 
 def key_for_min_value(name_hash)
   if !hash.empty?
-    min_hash = {}
-    name_hash.
+    min_hash = [name, Float::INFINITY]
+    name_hash.each do |name, value|
+      if value < min_hash[1]
+        min_hash[0], min_hash[1] = name, value
+      end
+      return min_hash[0]
   end
   return nil
 end
